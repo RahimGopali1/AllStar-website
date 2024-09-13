5f849f798852ef708e9031dcc8d09d060fc9f80f
RahimGopali1 <grahim18@tbc.edu.np>
2024-09-13 14:26:45 +0545

    navbar complete

---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-page.component.html',
+  styleUrl: './home-page.component.scss'
+})
+export class HomePageComponent {
+
+}
